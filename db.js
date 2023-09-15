8c282c75e218f19517aee22f170bc98bb53bfdef
anurag-github-repo <145045536+anurag-github-repo@users.noreply.github.com>
2023-09-15 07:38:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongouri = "mongodb+srv://ujjawal:ujjawal123@crud-app.bm61lxf.mongodb.net/myDatabase?retryWrites=true&w=majority" //see here database name
+const mongouri = ; //see here database name, enter link here
 const mongoConnect = mongoose.connect(mongouri, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('Connected to the database'); 
